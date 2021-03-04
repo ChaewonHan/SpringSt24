62a55539b340de94cd609435f84155f6b7fe1774
username <chwonhan00@gmail.com>
2021-03-04 22:23:01 +0900

    0304

--- src/main/java/com/mvc/josnexam/JsonController.java
+++ src/main/java/com/mvc/josnexam/JsonController.java
@@ -2,15 +2,20 @@ package com.mvc.josnexam;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class JsonController {
@@ -34,4 +39,57 @@ public class JsonController {
 	      }
 	      out.close();
 	   }
+	   
+	   @RequestMapping(value = "test2.do", method=RequestMethod.POST)
+	   @ResponseBody // 결과를 response 객체에 담아서 보내는 어노테이션
+	   public String test2Method(HttpServletResponse response) throws IOException{
+		   response.setContentType("application/json; charset=utf-8");
+		   
+		   JSONObject job = new JSONObject();
+		   // Map 형식의 JSON 객체를 생성하여 출력할 값을 Key와 Value 형태로 담는다
+		   job.put("no",123);
+		   job.put("title", "test return json object");
+		   
+		   // 한글 전송 시 깨질 우려가 있으므로 URIEncoder로 UTF-8 방식의 인코딩을 처리한다
+		   job.put("writer",URLEncoder.encode("홍길동","utf-8"));
+		   job.put("content",URLEncoder.encode("json 객체를 뷰로 리턴하는 테스트","utf-8"));
+		   
+		   //JSONObject를 String 형태로 리턴한다.
+		   return job.toJSONString();
+		   
+	   }
+	   
+	   @RequestMapping(value="test3.do", method=RequestMethod.POST)
+	   public void test3Method(HttpServletResponse response) throws IOException{
+	   logger.info("test3Method() run...");
+	   //List를 json 배열로 만들어서, 뷰로 리턴 처리한다.
+	   ArrayList<User> list = new ArrayList<User>();
+	   list.add(new User("u1234", "p1234", "홍길동", 25, "h1234@kh.org"));
+	   list.add(new User("u2345", "p2345", "박문수", 33, "p2345@kh.org"));
+	   list.add(new User("u3456", "p3456", "장영실", 45, "j3456@kh.org"));
+
+	   // 전송용 최종 json 객체
+	   JSONObject sendJson = new JSONObject();
+	   // JSONArray 객체를 생성하여 JSONObject 객체를 하나씩 담는다
+	   JSONArray jarr = new JSONArray();
+
+	   //list 를 jarr 에 저장 처리
+	   for(User user : list){
+	   //user 정보 저장할 json 객체 선언
+	   JSONObject juser = new JSONObject();
+	   juser.put("userId", user.getUserId());
+	   juser.put("userPwd", user.getUserPwd());
+	   juser.put("userName", URLEncoder.encode(user.getUserName(), "utf-8"));
+	   juser.put("age", user.getAge());
+	   juser.put("email", user.getEmail());
+	   jarr.add(juser);
+	   }
+	   // 전송할 객체 배열을 JSONObject에 담아 한 번에 전달한다
+	   sendJson.put("list", jarr);
+	   response.setContentType("application/json; charset=utf-8");
+	   PrintWriter out = response.getWriter();
+	   out.println(sendJson.toJSONString());
+	   out.flush();
+	   out.close();
+	  }
 }
--- src/main/webapp/WEB-INF/views/jsontest/test.jsp
+++ src/main/webapp/WEB-INF/views/jsontest/test.jsp
@@ -18,8 +18,61 @@
 					}else{
 						alert('전송실패');
 					}
+				},
+				error : function(request, satus, errorData){
+					alert("error code : "+request.status+"\n"
+							+"message : "+request.responseTest
+							+"\n"+"error: "+errorData);
 				}
 				
+			});
+		});
+		$('#test2').on('click',function(){
+				$.ajax({
+					url : 'test2.do',
+					type: 'post',
+					dataType: 'json',
+					success : function(data){
+						$('#d2').html(
+								"번호 : "+data.no
+								+"<br>제목: "+data.title
+								+"<br>작성자: "+decodeURIComponent(data.writer)
+								+"<br>내용:"+decodeURIComponent(data.content.replace(/\+/g, " "))
+								);
+					},
+					error : function(request, satus, errorData){
+						alert("error code : "+request.status+"\n"
+								+"message : "+request.responseTest
+								+"\n"+"error: "+errorData);
+					}
+				});
+		});
+		$('#test3').on('click', function(){
+			$.ajax({
+				url: 'test3.do',
+				type: 'post',
+				dataType: 'json',
+				success : function(data){
+					var jsonStr = JSON.stringify(data);
+					var json = JSON.parse(jsonStr);				
+					var values = $('#d3').html();
+
+					for(var i in json.list){
+						values += json.list[i].userId+", "
+						+json.list[i].userPwd+", "
+						+decodeURIComponent(json.list[i].userName)+", "
+						+json.list[i].age+", "
+						+json.list[i].email+"<br>"
+					}
+					
+					// valuse에 담은 값을 id가 d3인 div에 출력한다.
+					$('#d3').html(values);
+				},
+				error : function(request, satus, errorData){
+					alert("error code : "+request.status+"\n"
+							+"message : "+request.responseTest
+							+"\n"+"error: "+errorData);
+				}
 			});
 		});
 	});
@@ -28,5 +81,9 @@
 </head>
 <body>
 <input type="button" id="test1" value="ajax text1">
+<input type="button" id="test2" value="ajax text2">
+<input type="button" id="test3" value="ajax test3">
+<div id="d2"></div>
+<div id="d3"></div>
 </body>
 </html>
